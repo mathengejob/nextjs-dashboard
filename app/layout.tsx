24acfda84822f02fc1b3e9d8919e129c559ad10c
mathengejob <jobmathenge90@gmail.com>
2024-04-21 13:43:12 +0300

    Init nextjs

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import '@/app/ui/global.css';
+import { inter} from './ui/font';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialised`}>{children}
+      </body>
+      
+    </html>
+  );
+}
